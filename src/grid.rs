e02c3b81075ea799d5a951d3f3a5c5cbb178a58d
Nilson Sampath J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-06-30 21:32:33 -0700

    Implement LayerDimensions struct

--- src/grid.rs
+++ src/grid.rs
@@ -1,9 +1,9 @@
-use taser_em_shaders::fdtd1::PmlCoefficients;
 use crate::ElectricMaterial;
 use glamx::Vec3;
 use parry3d::bounding_volume::{Aabb, BoundingVolume};
 use parry3d::math::Pose;
 use parry3d::shape::{Cuboid, SharedShape};
+use taser_em_shaders::fdtd1::PmlCoefficients;
 use taser_em_shaders::math::{vec3_to_vect, GridIndex, Vect, VectExt, DIM};
 
 pub struct MaterialRegions {
@@ -65,11 +65,11 @@ impl YeeGrid {
         }
     }
 
-    pub fn update_coeffs_pml(&self, _dims: PmlDimensions, _n_cells: GridIndex) -> Vec<PmlCoefficients> {
+    pub fn update_coeffs_pml(&self, _pml_dims: LayerDimensions, _n_cells: GridIndex) -> Vec<PmlCoefficients> {
         todo!()
     }
 
-    /// Clears all stored material properties and material regions
+    /// Clears all stored material properties
     pub fn clear(&mut self) {
         self.mu_r.clear();
         self.eps_r.clear();
@@ -80,16 +80,109 @@ impl YeeGrid {
     // normal map creation?
 }
 
-pub struct PmlDimensions {
+/// The widths of layers of cells at the boundary of the simulation. (e.g. PMLs)
+///
+/// Stores the "low" and "high" widths on each axis.
+#[derive(Copy, Clone)]
+pub struct LayerDimensions {
     pub widths: [[u32; 2]; DIM],
 }
 
-impl PmlDimensions {
-    // TODO: a new() function for each dim feature?
+impl LayerDimensions {
+    #[inline]
     #[cfg(feature = "dim1")]
     pub fn new(z_lo: u32, z_hi: u32) -> Self {
         Self {
             widths: [[z_lo, z_hi]]
+        }
+    }
+
+    #[inline]
+    #[cfg(feature = "dim2")]
+    pub fn new(x_lo: u32, x_hi: u32, y_lo: u32, y_hi: u32) -> Self {
+        Self {
+            widths: [[x_lo, x_hi], [y_lo, y_hi]]
+        }
+    }
+
+    #[inline]
+    #[cfg(feature = "dim3")]
+    pub fn new(x_lo: u32, x_hi: u32, y_lo: u32, y_hi: u32, z_lo: u32, z_hi: u32) -> Self {
+        Self {
+            widths: [[x_lo, x_hi], [y_lo, y_hi], [z_lo, z_hi]]
+        }
+    }
+
+    #[inline]
+    pub fn iter_axes(&self) -> impl Iterator<Item=(AxisIndex, &[u32; 2])> {
+        self.widths.iter()
+            .enumerate()
+            .map(|(axis, lo_hi)| (AxisIndex::try_from(axis).unwrap(), lo_hi))
+    }
+
+    #[inline]
+    pub fn iter_axes_mut(&mut self) -> impl Iterator<Item=(AxisIndex, &mut [u32; 2])> {
+        self.widths.iter_mut()
+            .enumerate()
+            .map(|(axis, lo_hi)| (AxisIndex::try_from(axis).unwrap(), lo_hi))
+    }
+}
+
+impl core::ops::Add for LayerDimensions {
+    type Output = Self;
+    fn add(mut self, rhs: Self) -> Self::Output {
+        for ([lo1, hi1], [lo2, hi2]) in self.widths.iter_mut()
+            .zip(rhs.widths.iter())
+        {
+            *lo1 += lo2;
+            *hi1 += hi2;
+        }
+        self
+    }
+}
+
+impl core::ops::Index<AxisIndex> for LayerDimensions {
+    type Output = [u32; 2];
+    fn index(&self, index: AxisIndex) -> &Self::Output {
+        &self.widths[index as usize]
+    }
+}
+
+impl core::ops::IndexMut<AxisIndex> for LayerDimensions {
+    fn index_mut(&mut self, index: AxisIndex) -> &mut Self::Output {
+        &mut self.widths[index as usize]
+    }
+}
+
+#[repr(usize)]
+#[cfg(any(feature = "dim2", feature = "dim3"))]
+pub enum AxisIndex {
+    X = 0,
+    Y = 1,
+    Z = 2,
+}
+#[cfg(feature = "dim1")]
+pub enum AxisIndex {
+    Z = 0,
+}
+
+impl TryFrom<usize> for AxisIndex {
+    type Error = ();
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        #[cfg(any(feature = "dim2", feature = "dim3"))]
+        match value {
+            0 => Ok(AxisIndex::X),
+            1 => Ok(AxisIndex::Y),
+            2 => Ok(AxisIndex::Z),
+            _ => Err(()),
+        }
+
+        #[cfg(feature = "dim1")]
+        if value == 0 {
+            Ok(AxisIndex::Z)
+        }
+        else {
+            Err(())
         }
     }
 }
