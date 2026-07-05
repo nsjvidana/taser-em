bd81ba88186a5199967eda25f346eb852b3e47e6
Nilson J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-07-05 16:44:41 -0700

    More GridIndex/Vect util functions

--- crates/shaders/src/math.rs
+++ crates/shaders/src/math.rs
@@ -1,8 +1,10 @@
-use khal_std::glamx::Vec3;
+use khal_std::glamx::{UVec3, Vec3, Vec4};
 pub use dim_types::*;
 
 pub type Real = f32;
 pub type Index = u32;
+
+pub const MAX_DIM: usize = 3;
 
 #[cfg(feature = "dim1")]
 mod dim_types {
@@ -25,7 +27,7 @@ mod dim_types {
 #[cfg(feature = "dim3")]
 mod dim_types {
     use khal_std::glamx::{UVec3, Vec3};
-    pub const DIM: usize = 3;
+    pub const DIM: usize = super::MAX_DIM;
     /// A shader's vector field element
     pub type Vect = Vec3;
     pub type GridIndex = UVec3;
@@ -37,7 +39,7 @@ use khal_std::glamx::Vec3Swizzles;
 /// Converts a [`Vect`] to [`Vec3`]
 /// `mask` sets the components that `self` don't already have.
 ///
-/// e.g. `MathW<f32>::to_3d()` would return `Vec3::new(mask.x, mask.y, self.0)`
+/// e.g. `to_3d(z, Vec3::ONE)` would return `Vec3::new(1., 1., z)` in 1D.
 #[inline]
 #[allow(unused_variables)]
 pub fn to_3d(v: Vect, mask: Vec3) -> Vec3 {
@@ -47,6 +49,44 @@ pub fn to_3d(v: Vect, mask: Vec3) -> Vec3 {
     return mask.with_xy(v);
     #[cfg(feature = "dim3")]
     v
+}
+
+#[inline]
+pub fn vect_to_array(v: Vect) -> [Real; DIM] {
+    #[cfg(feature = "dim1")]
+    return [v];
+    #[cfg(not(feature = "dim1"))]
+    v.to_array()
+}
+
+#[inline]
+pub fn vec4_to_vect(v: Vec4) -> Vect {
+    #[cfg(feature = "dim1")]
+    return v.z;
+    #[cfg(feature = "dim2")]
+    {
+        use khal_std::glamx::Vec4Swizzles;
+        return v.xy();
+    }
+    #[cfg(feature = "dim3")]
+    {
+        use khal_std::glamx::Vec4Swizzles;
+        return v.xyz();
+    }
+}
+
+/// Converts a [`GridIndex`] to [`UVec3`]
+/// `mask` sets the components that `self` don't already have.
+///
+/// e.g. `grid_index_to_3d(k, UVec3::ONE)` would return `UVec3::new(1, 1, k)` in 1D.
+#[inline]
+pub fn grid_index_to_3d(idx: GridIndex, mask: UVec3) -> UVec3 {
+    #[cfg(feature = "dim1")]
+    return mask.with_x(idx);
+    #[cfg(feature = "dim2")]
+    return mask.with_xy(idx);
+    #[cfg(feature = "dim3")]
+    idx
 }
 
 /// Convert [`Vect`] to [`GridIndex`] using `as` keyword
@@ -84,6 +124,16 @@ pub fn vec3_to_vect(vec3: Vec3) -> Vect {
     return vec3.xy();
     #[cfg(feature = "dim3")]
     vec3
+}
+
+#[inline]
+pub fn uvec3_to_grid_index(uvec3: UVec3) -> GridIndex {
+    #[cfg(feature = "dim1")]
+    return uvec3.z;
+    #[cfg(feature = "dim2")]
+    return uvec3.xy();
+    #[cfg(feature = "dim3")]
+    uvec3
 }
 
 #[allow(unused_variables)]
