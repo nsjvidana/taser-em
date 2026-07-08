aae4a94dc35c4523948e32e4c017e11ab13b06b9
Nilson J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-07-08 10:24:49 -0700

    1D lossy FDTD benchmarking (no valid inputs)

--- crates/taser-em-testbed/src/main.rs
+++ crates/taser-em-testbed/src/main.rs
@@ -1,37 +1,46 @@
-use khal::backend::{Backend, GpuBackend, WebGpu};
+use khal::backend::{Backend, Encoder, GpuBackend, WebGpu};
 use khal::Shader;
-use kiss3d::prelude::*;
-use taser_em1d::shaders::fdtd1::GridParameters;
-use taser_em1d::{Fdtd1, Fdtd1Runner};
+use kiss3d::glamx::Vec4;
+use taser_em1d::gpu_util::CreateGpuBuffer;
+use taser_em1d::shaders::fdtd::{GridParameters2, IntegrationTerms, PmlCoefficients2};
+use taser_em1d::FdtdWithLoss;
 
 #[kiss3d::main]
 async fn main() {
-    let mut window = Window::new("taser FDTD test bed").await;
-    let mut camera = OrbitCamera3d::default();
-    let mut scene = SceneNode3d::empty();
-    scene
-        .add_light(Light::point(100.0))
-        .set_position(Vec3::new(0.0, 2.0, -2.0));
-
     let webgpu = WebGpu::default().await.unwrap();
     let backend = GpuBackend::WebGpu(webgpu);
-    let kernel = Fdtd1::from_backend(&backend).unwrap();
-    let mut runner = Fdtd1Runner::new(&backend, 100, GridParameters { dz: 1. }).unwrap();
 
+    let n_cells = 100;
+    let mut h = vec![Vec4::default(); n_cells].create_gpu_buffer(&backend).unwrap();
+    let mut dn = vec![Vec4::default(); n_cells].create_gpu_buffer(&backend).unwrap();
+    let mut en = vec![Vec4::default(); n_cells].create_gpu_buffer(&backend).unwrap();
+    let mut int_terms = vec![IntegrationTerms::default(); n_cells].create_gpu_buffer(&backend).unwrap();
+    let grid_coeffs = vec![PmlCoefficients2::default(); n_cells].create_gpu_buffer(&backend).unwrap();
+    let grid = GridParameters2::default().create_gpu_uniform(&backend).unwrap();
+    let fdtd = FdtdWithLoss::from_backend(&backend).unwrap();
     let mut total = std::time::Duration::default();
-    let mut count = 0;
-    let max_count = 100;
-    while window.render_3d(&mut scene, &mut camera).await {
-        let _ = runner.submit(&backend, &kernel, None).unwrap();
-        let start = std::time::Instant::now();
-        backend.synchronize().unwrap();
-        let elapsed = start.elapsed();
-        total += elapsed;
-        count += 1;
-        if count >= max_count {
-            println!("{}ms", total.as_secs_f64() * 1000. / max_count as f64);
-            total = std::time::Duration::default();
-            count = 0;
+    let num_trials = 1000;
+    for _ in 0..num_trials {
+        let mut encoder = backend.begin_encoding();
+        {
+            let mut pass = encoder.begin_pass("fdtd", None);
+            for _ in 0..100 {
+                fdtd.call(
+                    &mut pass,
+                    n_cells,
+                    &mut h,
+                    &mut dn,
+                    &mut en,
+                    &mut int_terms,
+                    &grid_coeffs,
+                    &grid,
+                ).unwrap();
+            }
         }
+        let start = std::time::Instant::now();
+        backend.submit(encoder).unwrap();
+        backend.synchronize().unwrap();
+        total += start.elapsed();
     }
+    println!("Average execution time: {:?}", total / num_trials);
 }
