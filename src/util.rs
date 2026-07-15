75067468e8661783422c3d43e50c2a3253bcad9b
Nilson J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-07-15 13:22:59 -0700

    Remove manual map in generate_mesh()

--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,15 @@
 /// Attempt to generate a mesh from an arbitrary shape
 #[cfg(feature = "render")]
 pub fn generate_mesh(shape: &dyn parry3d::shape::Shape) -> Option<(Vec<glamx::Vec3>, Vec<[u32; 3]>)> {
-    if let Some(ball) = shape.as_ball() {
-        Some(ball.to_trimesh(8, 8))
-    }
-    else if let Some(cuboid) = shape.as_cuboid() {
-        Some(cuboid.to_trimesh())
-    }
-    else if let Some(caps) = shape.as_capsule() {
-        Some(caps.to_trimesh(8, 8))
-    }
-    else if let Some(mesh) = shape.as_trimesh() {
-        Some((mesh.vertices().to_vec(), mesh.indices().to_vec()))
-    }
-    else { None }
+    shape.as_ball()
+        .map(|ball| ball.to_trimesh(8, 8))
+        .or_else(|| shape.as_cuboid()
+            .map(|cuboid| cuboid.to_trimesh())
+        )
+        .or_else(|| shape.as_capsule()
+            .map(|caps| caps.to_trimesh(8, 8))
+        )
+        .or_else(|| shape.as_trimesh()
+            .map(|mesh| (mesh.vertices().to_vec(), mesh.indices().to_vec()))
+        )
 }
