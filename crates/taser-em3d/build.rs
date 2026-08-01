eb0b18fefbcc6ae6259a2b0b9453fe05e4fffd0c
Nilson J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-08-01 16:16:41 -0700

    rename shader src dir to src_shaders

--- crates/taser-em3d/build.rs
+++ crates/taser-em3d/build.rs
@@ -3,7 +3,7 @@ use khal_builder::KhalBuilder;
 
 fn main() {
     let output_dir = PathBuf::from(std::env::var_os("OUT_DIR").expect("OUT_DIR not set by cargo"))
-        .join("shaders-spirv");
+        .join("src_shaders-spirv");
 
     KhalBuilder::from_dependency("taser-em-shaders3d", true)
         .feature("dim3")
