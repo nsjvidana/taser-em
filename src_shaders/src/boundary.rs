a9f6cc36a548ca0c15eca9b42f3db8c15594fc75
Nilson J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-08-23 19:12:51 -0700

    Add new PEC boundary shaders

--- src_shaders/src/boundary.rs
+++ src_shaders/src/boundary.rs
@@ -27,3 +27,39 @@ pub fn gpu_pec_boundary(
     dn.write(idx, Vec4::ZERO);
     en.write(idx, Vec4::ZERO);
 }
+
+macro_rules! pec_boundary {
+    ($kernel_name:ident, $axis:ident) => {
+        #[spirv_bindgen]
+        #[cfg_attr(feature = "dim1", spirv(compute(threads(1, 1, 64))))]
+        #[cfg_attr(feature = "dim2", spirv(compute(threads(8, 8, 1))))]
+        #[cfg_attr(feature = "dim3", spirv(compute(threads(4, 4, 4))))]
+        pub fn $kernel_name(
+            #[spirv(global_invocation_id)] cell_idx3: UVec3,
+            #[spirv(uniform, descriptor_set = 0, binding = 0)] grid: &GridParameters,
+            // Vector fields
+            #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] h: &mut [Vec4],
+            #[spirv(storage_buffer, descriptor_set = 0, binding = 2)] dn: &mut [Vec4],
+            #[spirv(storage_buffer, descriptor_set = 0, binding = 3)] en: &mut [Vec4],
+        ) {
+            let cell_idx = GridIndex::from_uvec3(cell_idx3);
+            let n_cells = GridIndex::from_uvec3(grid.n_cells3);
+            let lo_boundary = cell_idx3.$axis == 0;
+            let hi_boundary = cell_idx3.$axis == (grid.n_cells3 - 1).$axis;
+            let out_of_bounds = cell_idx3.cmpge(grid.n_cells3).any();
+            if !(lo_boundary || hi_boundary) || out_of_bounds { return; }
+        
+            let idx = cell_idx.to_flat_idx(n_cells) as usize;
+            h.write(idx, Vec4::ZERO);
+            dn.write(idx, Vec4::ZERO);
+            en.write(idx, Vec4::ZERO);
+        }
+    };
+}
+
+#[cfg(not(feature = "dim1"))]
+pec_boundary!(gpu_pec_boundary_x, x);
+#[cfg(not(feature = "dim1"))]
+pec_boundary!(gpu_pec_boundary_y, y);
+#[cfg(not(feature = "dim2"))]
+pec_boundary!(gpu_pec_boundary_z, z);
