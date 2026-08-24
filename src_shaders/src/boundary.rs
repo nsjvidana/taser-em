922b69b05ed48b0f00ea55d300ab769f31ac8cbd
Nilson J Vidanapatiranage <nilsonjvidana@gmail.com>
2026-08-23 17:48:22 -0700

    Implement periodic in X axis

--- src_shaders/src/boundary.rs
+++ src_shaders/src/boundary.rs
@@ -27,3 +27,51 @@ pub fn gpu_pec_boundary(
     dn.write(idx, Vec4::ZERO);
     en.write(idx, Vec4::ZERO);
 }
+
+#[cfg(not(feature = "dim1"))]
+#[spirv_bindgen]
+#[cfg_attr(feature = "dim1", spirv(compute(threads(1, 1, 64))))]
+#[cfg_attr(feature = "dim2", spirv(compute(threads(8, 8, 1))))]
+#[cfg_attr(feature = "dim3", spirv(compute(threads(4, 4, 4))))]
+pub fn gpu_periodic_x_en(
+    #[spirv(global_invocation_id)] idx3: UVec3,
+    #[spirv(uniform, descriptor_set = 0, binding = 0)] grid: &GridParameters,
+    // Vector fields
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] en: &mut [Vec4],
+) {
+    let cell_idx = GridIndex::from_uvec3(idx3);
+    let n_cells = GridIndex::from_uvec3(grid.n_cells3);
+    let last_idx = n_cells.x - 1;
+    let hi_boundary = cell_idx.x == last_idx;
+    let out_of_bounds = idx3.cmpge(grid.n_cells3).any();
+    if !hi_boundary || out_of_bounds { return; }
+
+    let idx = cell_idx.to_flat_idx(n_cells) as usize;
+    
+    let en_lo = en.read(idx - (grid.flat_idx_incrs.x * last_idx) as usize);
+    en.write(idx, en_lo);
+}
+
+#[cfg(not(feature = "dim1"))]
+#[spirv_bindgen]
+#[cfg_attr(feature = "dim1", spirv(compute(threads(1, 1, 64))))]
+#[cfg_attr(feature = "dim2", spirv(compute(threads(8, 8, 1))))]
+#[cfg_attr(feature = "dim3", spirv(compute(threads(4, 4, 4))))]
+pub fn gpu_periodic_x_h(
+    #[spirv(global_invocation_id)] idx3: UVec3,
+    #[spirv(uniform, descriptor_set = 0, binding = 0)] grid: &GridParameters,
+    // Vector fields
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] h: &mut [Vec4],
+) {
+    let cell_idx = GridIndex::from_uvec3(idx3);
+    let n_cells = GridIndex::from_uvec3(grid.n_cells3);
+    let lo_boundary = cell_idx.x == 0;
+    let last_idx = n_cells.x - 1;
+    let out_of_bounds = idx3.cmpge(grid.n_cells3).any();
+    if !lo_boundary || out_of_bounds { return; }
+
+    let idx = cell_idx.to_flat_idx(n_cells) as usize;
+
+    let h_hi = h.read(idx + (grid.flat_idx_incrs.x * last_idx) as usize);
+    h.write(idx, h_hi);
+}
